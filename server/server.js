10baeb15c7986bc204e5ec4d2e8592c70d82f6d3
EmilioSG23 <eisaenz@espol.edu.ec>
2024-01-14 21:27:26 -0500

    Peticiones completadas de grupo de operadores y registro de usuario

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ app.get("/getTouristGroups",(req,res)=>{
 })
 
 app.post("/createTouristGroup",(req,res)=>{
-
     try{
     const { codigo, members, rol } = req.body;
     const groupsRef = db.ref('groups');
@@ -44,12 +43,51 @@ app.post("/createTouristGroup",(req,res)=>{
 
 
 
-app.get("/operatorGroups/:id", (req,res)=>{
-
+app.get("/operatorGroups/", (req,res)=>{
+    try {
+        const groupsRef = db.ref('groups');
+    
+        groupsRef.once('value', (info) => {
+          const groups = info.val() || {};
+          
+          const operatorGroups = Object.values(groups).filter(group => group && group.rol === 2);
+    
+          res.status(200).json(operatorGroups);
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error con la peticion' });
+      }
 })
 
-app.post("register/", async (req,res)=>{
+app.get("/operatorGroups/:id", (req,res)=>{
+    try{
+        db.ref("groups").once("value", (info) => {
+            const groups = info.val() || {}
+            const groupSelected = groups[req.params.id];
 
+            if(groupSelected != null)
+                if(groupSelected["rol"] == "2")
+                    res.status(200).json(groupSelected);
+                else
+                    res.status(400).json({message: "El grupo no es de operadores"})
+            else
+                res.status(400).json({message: "No se encontró el grupo"});
+        })
+    }catch(error){
+        res.status(400).send(error)
+    }
+})
+
+app.post("/register", (req,res)=>{
+    try{
+        const {groups, rol, user} = req.body;
+        const users = db.ref("users");
+        users.push({user,rol,groups});
+        res.status(200).json({success: true, message: "Registro exitoso"});
+    }catch(error){
+        res.status(400).send(error)
+    }
 })
 
 app.listen(5000,()=>{console.log("Servidor iniciado en el puerto 5000")})
